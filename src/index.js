dfe3292c26259ab57c53daa14ae8b060da7ec8d4
Максим Коровин <maxim.korovin@russianpost.ru>
2018-08-13 22:27:28 +0300

    форма с подтверждением телефона

--- src/index.js
+++ src/index.js
@@ -23,9 +23,7 @@ var data = {};
 window.addEventListener(
   'load',
   function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
     Array.prototype.filter.call(forms, function(form) {
       form.addEventListener(
         'submit',
