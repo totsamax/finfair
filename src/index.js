51f1db77f9912452820c574fc10e3c175acd7c4d
Maxim Korovin <totsamax@gmail.com>
2018-08-13 20:27:26 +0300

    1208

--- src/index.js
+++ src/index.js
@@ -19,27 +19,6 @@ $('#lastName,#firstName,#patronymicName').mask(
 
 var regForm = $('#regForm');
 var data = {};
-$.each(regForm[0], function(k, v) {
-  if (v.id != 'btnSubmit') {
-    data[v.id] = v.value;
-  }
-});
-data = JSON.stringify(data);
-console.log(data);
-// var settings = {
-//   async: true,
-//   crossDomain: true,
-//   url: 'https://mainserver.accredcenter.ru/api/app/formdata/import/13',
-//   method: 'PUT',
-//   headers: {
-//     'X-API-Key':
-//       '997d223bc24b9afd1ccec8fd7dd66e200da4b2eccecabd14e039f4d79e00c61139daa6e7864e0c87aa3813180d6e86c3',
-//     'Content-Type': 'application/x-www-form-urlencoded',
-//     'Cache-Control': 'no-cache',
-//     'Postman-Token': 'a778497f-9d04-4ff7-ab0e-7d9a965fcf15',
-//   },
-//   data: data,
-// };
 
 window.addEventListener(
   'load',
@@ -56,7 +35,36 @@ window.addEventListener(
             event.stopPropagation();
           } else {
             event.preventDefault();
-            alert(data);
+            $.each(regForm[0], function(k, v) {
+              if (v.id != 'btnSubmit') {
+                data[v.id] = v.value;
+              }
+            });
+            data = JSON.stringify(data);
+            console.log(data);
+            var settings = {
+              async: true,
+              crossDomain: true,
+              url: 'https://finfair2018.ru/webreg.php',
+              method: 'POST',
+              headers: {
+                'X-API-Key':
+                  '997d223bc24b9afd1ccec8fd7dd66e200da4b2eccecabd14e039f4d79e00c61139daa6e7864e0c87aa3813180d6e86c3',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cache-Control': 'no-cache',
+                'Postman-Token': 'a778497f-9d04-4ff7-ab0e-7d9a965fcf15',
+              },
+              data: data,
+            };
+            $.ajax(settings)
+              .done(function(response) {
+                console.log(response);
+                $(regForm).html('<h3>Вы успешно зарегистрированы!</h3>');
+              })
+              .always(function() {
+                $(regForm).html('<h3>Вы успешно зарегистрированы!</h3>');
+              });
+
             $.each(regForm[0], function(k, v) {
               if (v && v.id && v.id != 'btnSubmit' && v.value) {
                 v.value = '';
@@ -71,6 +79,3 @@ window.addEventListener(
   },
   false
 );
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
